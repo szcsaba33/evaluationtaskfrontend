4c1a3cbf67954975c122d925756ea652b7487566
N2505 <n2505@student.jamk.fi>
2019-11-14 21:22:36 +0200

    serve-static a package.json-ban

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.get(/.*/, function (req, res) {
   res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 8080
 app.listen(port)
 console.log(`app is listening on port: ${port}`)
