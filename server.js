a1a054c3fefe3789a92d1c498273d7b3dbcff37d
N2505 <n2505@student.jamk.fi>
2019-11-14 22:31:26 +0200

    serverjs

--- server.js
+++ server.js
@@ -11,6 +11,19 @@ app.get(/.*/, function (req, res) {
   res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
+app.all('/*', function (req, res, next) {
+  // CORS headers
+  res.header('Access-Control-Allow-Origin', '*') // restrict it to the required domain
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  // Set custom headers for CORS
+  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key')
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+  } else {
+    next()
+  }
+})
+
 const port = process.env.PORT || 8080
 app.listen(port)
 console.log(`app is listening on port: ${port}`)
