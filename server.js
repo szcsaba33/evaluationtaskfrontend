d227cec7958232840468810b3b2fc2c96982594b
N2505 <n2505@student.jamk.fi>
2019-11-14 23:08:05 +0200

    some

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
-var cors = require('cors')
 
 const app = express()
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-app.use(cors)
-
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function (req, res) {
+  res.header('Access-Control-Allow-Origin', 'YOUR-DOMAIN.TLD') // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
