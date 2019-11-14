9b6845fdafc7550cdfe923aa34801ca634af1964
N2505 <n2505@student.jamk.fi>
2019-11-14 23:11:10 +0200

    some

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function (req, res) {
-  res.header('Access-Control-Allow-Origin', 'YOUR-DOMAIN.TLD') // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://backendquiz.herokuapp.com') // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
