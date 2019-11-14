4a23dc8ea283343841957b455189569e293bd710
N2505 <n2505@student.jamk.fi>
2019-11-14 22:36:23 +0200

    serverjs

--- server.js
+++ server.js
@@ -11,17 +11,19 @@ app.get(/.*/, function (req, res) {
   res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
-app.all('/*', function (req, res, next) {
+app.all('/*', function (req, res) {
   // CORS headers
   res.header('Access-Control-Allow-Origin', '*') // restrict it to the required domain
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   // Set custom headers for CORS
   res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key')
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-  } else {
-    next()
-  }
+})
+app.get('/*', function (req, res) {
+  // CORS headers
+  res.header('Access-Control-Allow-Origin', '*') // restrict it to the required domain
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  // Set custom headers for CORS
+  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key')
 })
 
 const port = process.env.PORT || 8080
