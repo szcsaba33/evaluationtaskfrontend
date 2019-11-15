f1f8d5cac3dc79a75d824639eb701b47647af288
Csaba <szilagyicsnandor@gmail.com>
2019-11-15 13:11:47 +0200

    próba3 törlés

--- server.js
+++ server.js
@@ -6,16 +6,11 @@ const app = express()
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.ORIGIN || '*')
-  next()
-})
-
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function (req, res) {
   res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
-const port = process.env.PORT || 8080 || 3001
+const port = process.env.PORT || 8080
 app.listen(port)
 console.log(`app is listening on port: ${port}`)
