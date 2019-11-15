a60121c10a9c782a469b19a10ff661b6d41549fa
N2505 <n2505@student.jamk.fi>
2019-11-15 13:04:47 +0200

    próba3

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
-var cors = require('cors')
 
 const app = express()
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', process.env.ORIGIN || '*')
+  next()
+})
 
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function (req, res) {
   res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080 || 3001
 app.listen(port)
 console.log(`app is listening on port: ${port}`)
